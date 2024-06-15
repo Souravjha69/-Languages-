35e163033673703d9af5eedad0259d628142b84e
Sourav69 <souravkumarjha301@gmail.com>
2024-06-15 15:18:40 +0530

    Variables basics finished

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -1,1 +1,18 @@
-console.log("Sourav kumar jha");
+let fullname = "Sourav kumar jha";
+console.log (fullname) ; 
+
+// Javascript is dynamically typed language here : 
+age = 24;  // {=} Equal to means here Assignment oprator means 24 age ke andr jake store ho gyi hai . 
+price = 99.9;
+console.log (age);
+console.log (price);
+
+x = null; // Iska mtlb x ke andr value to hai , but khali hai ye :
+console.log (x);
+y = undefined; // Iska mtlb , value y me pata hi nahi hai :
+console.log (y);
+
+isFollow = true; // In this we store only two types of value true or false , only these values.
+console.log (isFollow);
+isFollow2 = false; // We store only two types of Values here.
+console.log (isFollow2);
