6351543cd4568c2aab9c30b1e71e8bda6453bc4f
Sourav69 <souravkumarjha301@gmail.com>
2024-06-15 23:00:45 +0530

    Practice question 1 done

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -18,3 +18,38 @@ isFollow2 = false; // We store only two types of Values here.
 console.log (isFollow2);
 Console = 25; // Here console is a reserved keyword :
 console.log (Console);
+
+// let price = 56;
+// console.log (typeof(price));
+
+// let price = 56; // Declares a variable 'price' and assigns it the value 56
+// console.log(typeof(price));
+
+const student = { // This is Objects here : 
+    fullName : "Sourav kumar jha",
+    age : 20,
+    cgpa : 8.0,
+    isPass : true,
+}
+console.log(student);
+console.log (student["fullName"]); // printing key value pairs :
+console.log (student.age);
+
+// ................................//
+//......Practice Question Q.1......//
+
+const Product = {
+    fullName : "Parker Jotter Standard CT Ball Pen",
+    color : "Black",
+    rating : 7002,
+    price : 270,
+    isDeal : true,
+    offerPercentage : 5,
+}
+console.log (Product);
+console.log (Product.rating);
+console.log(Product.fullName);
+console.log(Product["offerPercentage"]);
+console.log (Product.isDeal);
+Product.isDeal = false;
+console.log (Product.isDeal);// I'm updated the value just for practice here :
