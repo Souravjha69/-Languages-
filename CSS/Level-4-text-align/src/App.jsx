140814255118eac489a3173b61e07e83b5c6c89c
Sourav kumar jha <souravkumarjha301@gmail.com>
2024-08-03 22:14:51 +0530

    Folder's arranged here Level wise

--- CSS/Level-4-text-align/src/App.jsx
+++ CSS/Level-4-text-align/src/App.jsx
@@ -1,5 +1,5 @@
-// import PracticeSet from "./components/PracticeSet";
-// import TextAlign from "./components/textAlign";
+import PracticeSet from "./components/PracticeSet";
+import TextAlign from "./components/textAlign";
 
 import Boxmodel from "./components/BoxModel";
 import Practiceset from "./components/Practiceset2";
@@ -7,8 +7,8 @@ import Practiceset from "./components/Practiceset2";
 function App () {
   return (
     <div>
-      {/* <TextAlign/>
-      <PracticeSet/> */}
+      <TextAlign/>
+      <PracticeSet/>
       <Boxmodel/>
       <Practiceset/>
     </div>
