4dd8761820f4f66b49aa4b44bbf74030fc2c641f
Sourav kumar jha <souravkumarjha301@gmail.com>
2024-10-05 23:14:54 +0530

    Python Programming has been started

---
+++ Python/FirstProgram.py
@@ -0,0 +1,8 @@
+name = "Sourav kumar jha"
+age = 25
+price = 25.99
+
+# print(name, "Age is like :-", age, price) 
+print ("My name is :-", name)
+print ("My age is : -", age)
+print ("My instrument Price is like : -", price)
