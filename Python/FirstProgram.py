ad1fc0a49e156787a5624ce4e0775b3a928e8b30
Sourav kumar jha <souravkumarjha301@gmail.com>
2024-10-05 23:26:29 +0530

    Variables and types learning

--- Python/FirstProgram.py
+++ Python/FirstProgram.py
@@ -1,8 +1,22 @@
 name = "Sourav kumar jha"
 age = 25
 price = 25.99
-
+age2 = age 
 # print(name, "Age is like :-", age, price) 
 print ("My name is :-", name)
 print ("My age is : -", age)
 print ("My instrument Price is like : -", price)
+print ("Age 2 is like: -", age2)
+
+# Also Printing the type of Variables: -
+print(type(name))
+print(type(age))
+print(type(price))
+
+age = 65
+old = True
+a = None 
+
+print("Age is like: -",type(age))
+print("old is like: - ",type(old))
+print("a is like : -",type(a))
