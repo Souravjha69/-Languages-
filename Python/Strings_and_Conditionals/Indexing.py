986dcb06a01a5a74baff104bd1e747197da03a98
Sourav kumar jha <souravkumarjha301@gmail.com>
2024-10-06 22:39:46 +0530

    String finished

--- Python/Strings_and_Conditionals/Indexing.py
+++ Python/Strings_and_Conditionals/Indexing.py
@@ -7,3 +7,25 @@ print(str[5])
 #Slicing of the String
 sli=str[0:6]
 print(sli) #Here Starting index included but Ending index don't. 
+print(str[:6]) #[0:6] Starting from 0 automatically
+print(str[0:]) #[0:len[str]] starting from 0 but ended end of string
+
+#Negative slicing of the index
+str2="Swati"
+print(str2[-3:-1])
+
+#String Function's
+str3="I am learning Python from Apna College"
+print(str3.endswith("ege")) #It shows string's ends with ege or not if not then output will be false
+
+#Replace the string
+str4="I am learning Python"
+str4=str4.replace("Python","Javascript") #It replaces the original string
+print(str4.replace("Python","Javascript"))
+print(str4)
+
+#Finding the string
+print(str4.find("g")) #It returns the index of that string
+
+#Count the string
+print(str4.count("n"))
---
+++ Python/Strings_and_Conditionals/Practice.py
@@ -0,0 +1,7 @@
+name=input("Enter your name: ")
+print(name)
+print("Length of the string Name",len(name))
+
+#Find the occurence of $
+name2="Sourav$ kr$ jha$"
+print("Printing the count/Occurence",name2.count("$"))
