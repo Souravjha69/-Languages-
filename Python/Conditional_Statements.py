987d13f1bf8e2bd7239b5e7a0ddd68f6b175be83
Sourav kumar jha <souravkumarjha301@gmail.com>
2024-10-06 17:21:38 +0530

    Operators and conditional statements learnt

--- Python/Conditional_Statements.py
+++ Python/Conditional_Statements.py
@@ -28,3 +28,34 @@ elif marks>70 and marks<80:
     print("You scored C grade")
 else:
     print("You scored D grade FAIL")
+
+# Print Output for
+A = int(input("Enter the value of A: "))
+G = input("M/F: ")
+
+if (A == 1 or A == 2) and G == "M":
+    print("fee is 100")
+elif A == 3 or A == 4 or G == "F":
+    print("fee is 200")
+elif A == 5 and G == "M":
+    print("fee is 300")
+else:
+    print("no fee")
+
+# Single Line If / Ternary Operator
+food=input("Enter the food:-")
+eat="Yes It's Cake" if food=="Cake" else "no"
+print(eat)
+
+food2=input("Enter the second food2: -")
+print("Sweet") if food2=="cake" or food2=="jalebi" else print("not sweet")
+
+# Clever If / Ternary Operator
+age=int(input("Enter the value of the age: -"))
+vote=("No you don't have right to vote", "Yes you can vote")[age<=18] 
+print(vote)
+
+sal = float(input("Salary: "))
+tax = sal * (0.2 if sal > 50000 else 0.1)
+print("Tax:", tax)
+
---
+++ Python/Operators.py
@@ -0,0 +1,16 @@
+a=89
+b=45
+print(a==b) #False
+print(a!=b) #True
+print(a>=b) #True
+print(b<=a) #True
+
+#Assignment Operators
+num=10
+num+=10
+num-=2
+print("Num",num)
+
+#Logical operators
+print(not True) #Just opposite
+print(not False) #Just opposite
