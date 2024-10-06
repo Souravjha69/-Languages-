3025854ca339c2724e719a5aab66f748ea1918c2
Sourav kumar jha <souravkumarjha301@gmail.com>
2024-10-06 22:16:09 +0530

    Basics of python completed and strted strings Chapter 2

--- Python/Basics/Type_Casting.py
+++ Python/Basics/Type_Casting.py
@@ -26,3 +26,4 @@ num1=34
 num2=45
 sum=num1+num2
 print(sum)
+
---
+++ Python/Strings_and_Conditionals/Indexing.py
@@ -0,0 +1,9 @@
+#Indexing of the string
+str="Sourav kumar jha"
+ch=str[2]
+print(ch)
+print(str[5])
+
+#Slicing of the String
+sli=str[0:6]
+print(sli) #Here Starting index included but Ending index don't.
