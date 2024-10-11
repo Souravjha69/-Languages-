f09fd22f0e965c9d5cbf6b045e04606de454782d
Sourav kumar jha <souravkumarjha301@gmail.com>
2024-10-11 19:47:07 +0530

    Dictionary and sets started here

---
+++ Python/Dictionary&Set/Lecture_One.py
@@ -0,0 +1,11 @@
+info ={
+    "name": "Sourav kumar jha",
+    "Company Name": "CyberSigma Consulting Services",
+    "Position":"Software Developer",
+    "Subjects":["Java", "Python","Cpp" ],
+    "Numbers":(24,55,44,55),
+    34:25.5
+}
+print("Printing the value of Key and Value Pairs: ", info)
+
+#Now this is the example of key value pairs also "name" is the key "Sourav kumar jha" is a pair. 
