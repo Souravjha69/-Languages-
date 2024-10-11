5ce27f10d81237d86928f1589bd8fdcfcbb9d0c8
Sourav kumar jha <souravkumarjha301@gmail.com>
2024-10-11 19:54:04 +0530

    Add or access the value in dictionary

--- Python/Dictionary&Set/Lecture_One.py
+++ Python/Dictionary&Set/Lecture_One.py
@@ -9,3 +9,12 @@ info ={
 print("Printing the value of Key and Value Pairs: ", info)
 
 #Now this is the example of key value pairs also "name" is the key "Sourav kumar jha" is a pair. 
+# Dictionary is mutable.
+# We can create duplicate keys. 
+
+#If we have to access the value of the dictionary here: -
+print(info["name"])
+
+#If we have to add new key in dictionary : -
+info["Dream"]="Japan"
+print(info)
