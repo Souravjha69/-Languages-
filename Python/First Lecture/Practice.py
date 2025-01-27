858f31b993c1c90ac7db68bcf4f691d6dc9d80a7
souravJha-cybersigmacs <163466710+Souravcybersigmacs69@users.noreply.github.com>
2025-01-27 15:03:03 +0530

    Python Chapter First Chapeter Completed

---
+++ Python/First Lecture/Practice.py
@@ -0,0 +1,24 @@
+#Question No. 01
+num1= int(input("Enter your numberOne:- "))
+num2= int(input("Enter your numberOne:- "))
+print("Sum of num1 and num2 will be like",num1+num2)
+
+#Question No. 02
+side= int(input("Enter the value of side:- "))
+side *=2
+Area=side
+print("Area of the square",Area)
+
+#Question No. 03
+num3=int(input("Enter your num3:- "))
+num4=int(input("Enter your num4:- "))
+AverageNo=int((num3+num4)/2)
+print("Now the Average no. will be",AverageNo) 
+print("Type if Average no:-",type(AverageNo))
+
+#Question No. 4
+num5=int(input("Enter your num5:- "))
+num6=int(input("Enter your num6:- "))
+print("If both are equal then true otherwise false:- ", num5==num6)
+print("If both are equal then true otherwise false:- ", num5>=num6)
+dfg
