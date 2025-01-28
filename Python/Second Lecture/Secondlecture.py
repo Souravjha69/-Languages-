b9761910b58ad9a2d2d28e527fbbaf62b49eec2b
souravJha-cybersigmacs <163466710+Souravcybersigmacs69@users.noreply.github.com>
2025-01-28 13:52:13 +0530

    Sring functions are completing

--- Python/Second Lecture/Secondlecture.py
+++ Python/Second Lecture/Secondlecture.py
@@ -20,3 +20,19 @@ str3="Apple"
 print(str3[-4:-1]) #This will print {ppl}
 
 #Starting the String functions
+str5="Sourav"
+print(str5.endswith("av")); #It prints True because it's checking last av is in the word or not if yes then it returns True. 
+
+str6="sourav"
+str6=str6.capitalize()
+print(str6) #It will capitalize the first letter of the str6.
+
+str7="My name is Sourav"
+print(str7.replace("Sourav","Varun")) #It will replace the Sourav to varun here. 
+
+str8="My subject is Python"
+print(str8.find("subject")) #It will find the first word of on the entire sentence
+
+str9="My name is Sourav kumar jha"
+print(str9.count("a")) #It counts the word in the sentence. 
+
