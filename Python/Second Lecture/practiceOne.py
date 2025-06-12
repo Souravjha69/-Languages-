f1ea9b131d4eb5a0461df77eced6cc6f7d37bda7
Sourav kumar jha <souravkumarjha301@gmail.com>
2025-06-12 20:58:54 +0530

    files deleted

--- Python/Second Lecture/practiceOne.py
+++
@@ -1,9 +0,0 @@
-# Question No : 01
-InputStr= input("Enter the name of the string :- ")
-print("This is the string that you take:- ", InputStr)
-print("This is the length of your string ",len(InputStr))
-
-
-#Question No: 02
-StrTwo= "L$iones$s$"
-print("Occurence of ", StrTwo.count("$"))
