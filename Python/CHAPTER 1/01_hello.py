abccda15ebf192614dd0e0230489b38bab8b5904
sourav <souravkumarjha301@gmail.com>
2025-07-21 00:14:39 +0530

    This is the comment and print also

---
+++ Python/CHAPTER 1/01_hello.py
@@ -0,0 +1,6 @@
+# Comment :- This is a simple Python script that prints "Hello, World!" to the console and this is the comment line.
+'''This is a multi-line comment
+and it can span multiple lines.
+'''
+import os
+print("Hello, World!")  # This line prints a greeting message to the console
