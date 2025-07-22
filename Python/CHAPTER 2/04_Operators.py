799c633c3583ad25109996b2f86348545884c473
sourav <souravkumarjha301@gmail.com>
2025-07-23 00:43:24 +0530

    Comparison operators

--- Python/CHAPTER 2/04_Operators.py
+++ Python/CHAPTER 2/04_Operators.py
@@ -9,3 +9,10 @@ print("This is the example of assignment operator:", a)
 b= 6
 b+= 45
 print("Here we increment the value of b:", b)
+b-= 10
+print("Here we decrement the value of b:", b)
+
+#Comparison Operators
+d = 10>5
+print("This is the example of comparison operator:", d)
+#Here comparison operators either return True or False
