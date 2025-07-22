d7bd36d59a4bcd3a209c31a91fdf2bed08694b59
sourav <souravkumarjha301@gmail.com>
2025-07-22 22:22:08 +0530

    operators

---
+++ Python/CHAPTER 2/03_Rule_Variables.py
@@ -0,0 +1,6 @@
+a= 23
+bbb= 45
+Sameer= 67
+_Sourav= 78
+# @Sourav= 78 This is not a valid variable name
+print("This is a variable new:", (a + bbb + Sameer + _Sourav))
---
+++ Python/CHAPTER 2/04_Operators.py
@@ -0,0 +1,11 @@
+#Arithmetic Operators
+a = 10
+b = 20
+print("This is a variable:", (a + b))
+
+#Assignment Operators
+a= 78-34
+print("This is the example of assignment operator:", a)
+b= 6
+b+= 45
+print("Here we increment the value of b:", b)
