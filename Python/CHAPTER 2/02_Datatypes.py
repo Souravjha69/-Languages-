018bb0e12fb1ee1a8b290fe3f4b7a67ea5a7516f
sourav <souravkumarjha301@gmail.com>
2025-07-22 21:49:44 +0530

    Chapter 3 is running

---
+++ Python/CHAPTER 2/02_Datatypes.py
@@ -0,0 +1,11 @@
+a= 67
+b= 98.67
+c=  "Sourav kumar jha"
+d=  True
+e=  None
+
+print("print this variable:", a)
+print("print this variable:", b)
+print("print this variable:", c)
+print("print this variable:", d)
+print("print this variable:", e)
---
+++ Python/CHAPTER 2/03_Variables.py
@@ -0,0 +1,3 @@
+a = 24
+b = 687
+print("This is a variable:",(a+b) )
