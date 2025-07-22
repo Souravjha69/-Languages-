54761f30836ba8b7480556f792f9ab8c4876b53e
sourav <souravkumarjha301@gmail.com>
2025-07-23 01:23:26 +0530

    Assignment in python

--- Python/CHAPTER 2/05_Type.py
+++ Python/CHAPTER 2/05_Type.py
@@ -1,2 +1,9 @@
-a = 34
-print("The type of variable a is:-", type(a)) #This is the example of type function
+#Assignment in Python
+a = 34 
+b = a
+print("This is a variable:", type(b))  # Here we assign the value of a to b
+
+c = "67.7"
+print("Type of c is:- ", type(c))
+e= float(c) #Here we are converting c string value to fload and placing that value in e 
+print("Type of e is: -", type(e)) 
