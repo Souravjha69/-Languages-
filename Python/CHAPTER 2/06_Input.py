885f4fc87a2fc0cc5518dc4d8f0d842c47791dab
sourav <souravkumarjha301@gmail.com>
2025-07-24 01:00:58 +0530

    Input part learning

--- Python/CHAPTER 2/06_Input.py
+++ Python/CHAPTER 2/06_Input.py
@@ -2,3 +2,9 @@
 a = int(input("Enter the value of a is:-"))
 b = int(input("Enter the value of b is:-"))
 print("The value of a + b is like this: -", (a+b))
+
+c = input("Enter the value of c is:-") 
+d = input("Enter the value of d is:-")
+print("The value of c is like:- ", c)
+print("The value of d is like:- ", d)
+# print("Type of c is like:- ", type(c)) This is defalt value of type is string. 
