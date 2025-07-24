30bd2910dd268a26dc5cadb8175ca83cf39db377
sourav <souravkumarjha301@gmail.com>
2025-07-24 21:59:25 +0530

    String functions

---
+++ Python/CHAPTER 3/02_str_functions.py
@@ -0,0 +1,5 @@
+Myname= "Souravkumarjha"
+
+print ("Length of my Full Name is like: -", len(Myname))
+print ("Is jha is present in my Full Name?: - ", (Myname.endswith("jha"))) # Here it is checking my name is ending with jha or not so it returns boolean value True or false 
+print ("Is Sourav name starts with my name: - ", (Myname.startswith("Sourav"))) # Here it is checking that my name is starting with "Sourav" or not so it returns boolean value True or false.
