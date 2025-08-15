e337094078cbb48622af3bf8ea3eb1911e91ea42
sourav <souravkumarjha301@gmail.com>
2025-08-15 18:48:11 +0530

    List is running right now

--- Python/CHAPTER 4/01_list.py
+++ Python/CHAPTER 4/01_list.py
@@ -10,3 +10,8 @@ print("Printing the changed 2nd item in the list:", Example[1]) #Printing the ch
 # Exmple of Indexing slicing here: -
 Example2 = ["Sourav", 69, "Python", 3.14, False, "Java", "Apple"]
 print("Here we slicing the list (Indexing):", Example2[1:4]) # As we know the last item in the list is not included in the slicing.
+
+Freinds = ['Sourav', "Kapil", 89, True, 3.1478, 'Python']
+Newfreinds = Freinds[1:4] # Here are we slicing the value and string the value only some extent from list.
+print("New Strings are printing here:", Newfreinds)
+# Here we create new strings from the freinds list thet is NewFreinds.
---
+++ Python/CHAPTER 4/02_list_methods.py
@@ -0,0 +1,12 @@
+Example = ["Sourav", 69, "Python", 3.14, True]
+print("Example list:", Example) #Whole index printing
+
+# Append method example means i can add a new item in the list.
+Example.append(False) # Addeing a new item in the list here "That is called append function"
+print("And here the item [False] is added is added ",Example)
+
+# Sort method example means i can sort the list items in ascending order.
+Example2 = [7,5,3,9,5,1,7,8,54,89]
+print("Sorting methiod example:", Example2)
+
+
