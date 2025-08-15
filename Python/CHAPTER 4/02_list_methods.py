22f394622c772a44fc53ac29668be912d80184f1
sourav <souravkumarjha301@gmail.com>
2025-08-15 21:13:04 +0530

    Ad

--- Python/CHAPTER 4/02_list_methods.py
+++ Python/CHAPTER 4/02_list_methods.py
@@ -8,5 +8,4 @@ print("And here the item [False] is added is added ",Example)
 # Sort method example means i can sort the list items in ascending order.
 Example2 = [7,5,3,9,5,1,7,8,54,89]
 print("Sorting methiod example:", Example2)
-
-
+ 
