bcaef8f06e0c70982b434bd67af29c5aa369ed42
sourav <souravkumarjha301@gmail.com>
2025-08-16 20:05:55 +0530

    Adds examples for insert, pop, and remove list methods
    Demonstrates how to insert, delete by index, and remove by value
    in a list, enhancing educational value with practical usage examples.

--- Python/CHAPTER 4/02_list_methods.py
+++ Python/CHAPTER 4/02_list_methods.py
@@ -19,3 +19,19 @@ print("Descending order is here follows:",Desc) # Descending order is here follo
 L3 = [24,58,7893,98, 1, -98 ]
 Reverse = L3.reverse()
 print("Here this is the reversed form of list: ", L3) #It's just reversed the list.
+
+# Here I'm showing the example of Inserting at fixed index
+insert = [1,67,98,56,98,-0]
+insert.insert(2, 69) # Here 2 is the index and 69 is inserting at 2nd index means 
+print("Inserted 69 index in the list: ", insert)
+
+#Here I'm deleting element ata particular index
+delete = [1,67,98,56,98,-0]
+delete.pop(2) # Here 2 is the index and it will delete the element at 2nd index
+print("Deleted element at index 2: ", delete) # Deleted the element at index 2
+print(delete.pop(2)) #Here it will print the deleted element at index 2
+
+#Here I'm showing the example of removing an element from the list
+remove = [1,67,98,56,98,-0]
+remove.remove(remove[4]) # Here it will remove specific element in the list and operating as their index
+print("Removed element 98 from the list:", remove)
