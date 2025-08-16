52bb997ad22a4300b395a7f59304143ca89c625d
sourav <souravkumarjha301@gmail.com>
2025-08-16 19:49:09 +0530

    Adds sorting and reversing examples for lists
    Expands list method demonstrations with sorting in ascending and descending order, as well as reversing, to clarify usage for learners.

--- Python/CHAPTER 4/02_list_methods.py
+++ Python/CHAPTER 4/02_list_methods.py
@@ -5,7 +5,17 @@ print("Example list:", Example) #Whole index printing
 Example.append(False) # Addeing a new item in the list here "That is called append function"
 print("And here the item [False] is added is added ",Example)
 
-# Sort method example means i can sort the list items in ascending order.
-Example2 = [7,5,3,9,5,1,7,8,54,89]
-print("Sorting methiod example:", Example2)
- 
+
+# Here I'm going to try the example of Sorting method
+L1 = [2,45,98,892, 1, -98 ]
+L2 = L1.sort()
+print("So here is the example of sorting method:,",L1) # Ascending order is here follows.
+
+Desc = [99,199,1,781,2,3,4,87]
+Desc.sort(reverse=True)
+print("Descending order is here follows:",Desc) # Descending order is here follows.
+
+
+L3 = [24,58,7893,98, 1, -98 ]
+Reverse = L3.reverse()
+print("Here this is the reversed form of list: ", L3) #It's just reversed the list.
