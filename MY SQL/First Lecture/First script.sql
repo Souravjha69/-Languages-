f4c91c10c0200bb6358b10008fdd76359e18bf46
sourav <souravkumarjha301@gmail.com>
2025-08-20 00:21:04 +0530

    Lecture script code updates add  and delete column

--- MY SQL/First Lecture/First script.sql
+++ MY SQL/First Lecture/First script.sql
@@ -9,5 +9,11 @@ date_of_birth date,
 created_by timestamp default current_timestamp
 );
 
-SELECT * from users;
-SELECT ID from users;
+SELECT * from users; -- If mujhe pura table hi dekhna ho then ye wali command
+SELECT ID from users;  -- If mujhe specifically ek hi column dekhna ho , command me then ye wali command
+SELECT ID, Email from users; -- If mujhe specifically 2 command table me dekhni ho the ye wali command 
+Rename table programmers to users; -- If i have to rename the table name the use this command
+ALTER TABLE users DROP COLUMN created_by; -- If i want to delete any column then use this command here 
+ALTER TABLE users ADD COLUMN created_at timestamp default current_timestamp; -- If i have to add any column in the table then use this command here
+Alter table users add column is_active boolean default true; -- If i have to add any column in the table then use this command here
+Select * from users;
