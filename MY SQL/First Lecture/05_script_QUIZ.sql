6d1e91c180b38ec3215012fbec3e510b2ca515b7
sourav <souravkumarjha301@gmail.com>
2025-08-29 00:09:58 +0530

    Changes

---
+++ MY SQL/First Lecture/05_script_QUIZ.sql
@@ -0,0 +1,8 @@
+-- Here this is the practice session here: - 
+
+-- This database I'm using right now: -
+USE  starter_sql; 
+
+SELECT * FROM users where salary > 60000 ORDER BY created_by DESC  LIMIT 5; -- In this list it is showing where i modified the data or entered the dats it sis showing in that form only.
+SELECT * FROM users order by salary desc; -- In this list it is showing only salary is descending order also I'm filtering any data just I'm viewing the data in descending order so I've used here order.
+SELECT * FROM users where Salary BETWEEN 50000 AND 70000; -- Here the salary is showing that lies in this value only.  
